c236dbc408c6559c9c10f566cdd3a2f3105c609e
rcd1 <70033436+rcd1@users.noreply.github.com>
2026-09-11 22:53:19 -0400

    i mightve added some comments to main.lua? i cant rember

--- pong/main.lua
+++ pong/main.lua
@@ -1,6 +1,10 @@
 -- bash like fucker
 -- your three are load, update, and draw. godot has ready and process which handles update and draw together.
-
+/*
+Amby found somethin' pretty neat here, thank you CS50! again...
+Now watch as she forgets this all in a matter of days, poor thing.
+remind her to shut off VS Code ghost writer, this is getting annoying.
+*/
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
