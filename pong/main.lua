d927a6fcdaa8e0caddea74a7e4c5ae7280000b37
rcd1 <70033436+rcd1@users.noreply.github.com>
2026-09-12 15:40:38 -0400

    learned about keypressed, how to quit the application, and filtering

--- pong/main.lua
+++ pong/main.lua
@@ -1,14 +1,16 @@
 -- bash like fucker
 -- your three are load, update, and draw. godot has ready and process which handles update and draw together.
-/*
-Amby found somethin' pretty neat here, thank you CS50! again...
-Now watch as she forgets this all in a matter of days, poor thing.
-remind her to shut off VS Code ghost writer, this is getting annoying.
-*/
+--Amby found somethin' pretty neat here, thank you CS50! again...
+-- Now watch as she forgets this all in a matter of days, poor thing.
+-- remind her to shut off VS Code ghost writer, this is getting annoying.
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = false,
         vsync = true, --syncing rendering to monitor refresh rate, very handy against screen tearing
@@ -18,6 +20,12 @@ end
 
 function love.update(dt)
 
+end
+-- Love input handling
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
 function love.draw()
