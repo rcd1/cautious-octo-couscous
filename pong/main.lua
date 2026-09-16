155c913f7cbf3db8106ada4c9c487dfe9f52070f
rcd1 <70033436+rcd1@users.noreply.github.com>
2026-09-16 12:57:54 -0400

    forgot zero state for paddles, how to require lua files without giving them a variable (for paddle and ball classes), to keep the variable naems consistent across methods, and to update paddles at end of turn

--- pong/main.lua
+++ pong/main.lua
@@ -3,6 +3,10 @@
 --Amby found somethin' pretty neat here, thank you CS50! again...
 -- Now watch as she forgets this all in a matter of days, poor thing.
 -- remind her to shut off VS Code ghost writer, this is getting annoying.
+push = require 'push'
+Class = require 'class'
+require 'Paddle'
+require 'Ball'
 
 
 -- global variables when capitalized like this
@@ -14,7 +18,6 @@ VIRTUAL_HEIGHT = 243
 
 PADDLE_SPEED = 200 --pixels per second
 
-push = require 'push'
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -24,21 +27,20 @@ function love.load()
         vsync = true, --syncing rendering to monitor refresh rate, very handy against screen tearing
         fullscreen = false
     })
-    -- upscale has some other properties
-    push.setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT, {upscale = 'normal'})
     
     largeFont = love.graphics.newFont('font.ttf', 32)
     smallFont = love.graphics.newFont('font.ttf', 8) 
-
     -- seeding rng
     math.randomseed(os.time())
-
-
+    -- upscale has some other properties
+    push.setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT, {upscale = 'normal'})
+    
     player1Score = 0
     player2Score = 0
 
     player1 = Paddle(10, 10, 5, 20)
-    player2 = Paddle(VIRTUAL_WIDTH-15, VIRTUAL_HEIGHT-30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH-10, VIRTUAL_HEIGHT-30, 5, 20)
+
     ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     gameState = 'start'
@@ -47,20 +49,27 @@ end
 function love.update(dt)
 
     if love.keyboard.isDown('w') then 
-        paddle1.dy = -PADDLE_SPEED
+        player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        paddle1.dy = PADDLE_SPEED
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
     end
 
     if love.keyboard.isDown('up') then 
-        paddle2.dy = -PADDLE_SPEED
+        player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        paddle2.dy = PADDLE_SPEED
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
     end
 
     if gameState == 'play' then 
         ball:update(dt)
     end
+
+    player1:update(dt)
+    player2:update(dt)
 
 end
 -- Love input handling
@@ -90,8 +99,8 @@ function love.draw()
     love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 - 80)
     love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 - 80)
     
-    paddle1:render()
-    paddle2:render()
+    player1:render()
+    player2:render()
     ball:render()
 
     push.finish()
